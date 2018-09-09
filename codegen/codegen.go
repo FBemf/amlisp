97585a77cebe1d8cc8cecf6cec84febe98a9e772
FBemf <felipe.m.p.bemfica@gmail.com>
2018-09-09 19:18:05 -0400

    Almost done regular function returns too

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -31,6 +31,19 @@ type safeSym struct {
         mutex sync.Mutex
 }
 
+func (sym *safeSym) getSymID(name string, counter func() int) (r int) {
+        sym.mutex.Lock()
+        if id, ok := sym.table[name]; ok {
+                r = id
+        } else {
+                r = counter()
+                sym.table[name] = r
+        }
+        sym.mutex.Unlock()
+        return
+}
+
+
 func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) {
 
         if p := ast.Primitive(); p != nil {
@@ -45,14 +58,7 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
                                 up <- assembly{"SET", r0, 0, 1} // refcount
                                 up <- assembly{"SET", r0, 1, Type_symbol} // type
                                 name := strconv.Atoi(p.Value())
-                                sym.mutex.Lock()
-                                if id, ok := sym.table[name]; ok {
-                                        up <- assembly{"SET", r0, 2, id)} // val
-                                } else {
-                                        sym.table[name] = counter()
-                                        up <- assembly{"SET", r0, 2, sym.table[name])} // val
-                                }
-                                sym.mutex.Unlock()
+                                up <- assembly{"SET", r0, 2, sym.getSymID(name, counter))} // val
                         default:
                                 fmt.Println("Unexpected primitive type!")
                         // TODO: add support for other primitives
@@ -70,7 +76,7 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
         if isFunc {
                 funcStart := counter()
                 funcEnd := counter()
-                up <- assembly{"JUMP", funcEnd, 0, 0}
+                up <- assembly{"JUMPLABEL", funcEnd, 0, 0}
                 up <- assembly{"LABEL", funcStart, 0, 0}
 
                 // Also get rid of the arg list
@@ -90,15 +96,12 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
         // Make an env for it
         // an env has refcount, type, length, saved pc, return loc, parent env, symbol table, pointers
 
-        // Note on symbols: A symbol is just a unique name. We'll need an internal function called RESOLVE
-        // that will dereference a symbol using the table and return a pointer directly to the value you want.
-        // DEFINE adds a new definition to the symbol table
-
         up <- assembly{"NEW", r2, members+7, 0}
         up <- assembly{"SETID", r0, 0, r2}
         up <- assembly{"SETI", r2, 0, 1}
         up <- assembly{"SETI", r2, 1, Type_environment}
-        up <- assembly{"SETI", r2, 2, members+4}
+        up <- assembly{"SETI", r2, 2, members+7}
+        up <- assembly{"SETID", r2, 3, r3}
         up <- assembly{"SETID", r2, 4, r0}
         up <- assembly{"SETID", r2, 5, r1}
         up <- assembly{"SETD", r3, r1, 6}
@@ -134,7 +137,8 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
 
                 up <- assembly{"SUB1I", r2, 0, 0}       // decrement refcount
 
-                up <- assembly{"SETD", r4, r2, 0}
+                up <- assembly{"SETD", r4, r2, 0}       // the argument for dumpfunc
+
                 // Ascend the registers to the previous environment
                 up <- assembly{"SETD", r2, r1, 0}
                 up <- assembly{"SETD", r1, r2, 5}
@@ -152,8 +156,66 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
                 up <- assembly{"JUMPD", r3, 0, 0}      // jump back
                 up <- assembly{"LABEL", funcEnd, 0, 0}
 
-                // TODO: Should return a closure structure
+                // Count number of args
+                args := 0
+                for t := argAst.Node(); t != nil; t = t.Next().Node() {
+                        if t.This() != nil {
+                                args++
+                        }
+                }
+
+                // create and populate a closure to return
+                up <- assembly{"NEW", r3, args+4, 0}
+                up <- assembly{"SETI", r3, 0, 1}
+                up <- assembly{"SETI", r3, 1, funcStart}
+                up <- assembly{"SETID", r3, 2, r2}
+                up <- assembly{"SETID", r3, 3, args+4}
+                for i := 0; i < args; i++ {
+                        up <- assembly{"NEW", r4, 3, 0}
+                        up <- assembly{"SETI", r4, 0, 1}
+                        up <- assembly{"SETI", r4, 1, Type_symbol}
+                        up <- assembly{"SETI", r4, 2, getSymID(argAst.Node().This().Primitive().Value(), counter)}
+                        up <- assembly{"SETID", r3, 4+i, r4}
+                }
+
+                up <- assembly{"SETID", r0, 0, r3}      // return it
+
+                // Ascend the registers to the previous environment
+                // BTW, rule of thumb for registers: r0 is where to write the return val,
+                // r1 is the location of the parent environment, r2 is the location of the
+                // current environment
+                up <- assembly{"SETD", r2, r1, 0}
+                up <- assembly{"SETD", r1, r2, 5}
+                up <- assembly{"SETD", r1, r1, 0}
+                up <- assembly{"SETD", r0, r2, 4}
+                up <- assembly{"SETD", r0, r0, 0}
+
         } else {
+                up <- assembly{"SETD", r3, r2, 7}       // grab address with ptr to func sym
+                up <- assembly{"SETD", r3, r3, 0}       // grab address of func sym
+                up <- assembly{"SETD", r3, r3, 2}       // grab address of sym id
+                up <- assembly{"SETD", r3, r3, 0}       // grab sym id
+
+                up <- assembly{"SETD", r4, r2, 6}       // grab sym table loc
+                up <- assembly{"SETD", r4, r4, 0}       // grab sym table
+
+                loop := counter()
+                end := counter()
+                up <- assembly{"LABEL", loop, 0, 0}
+                up <- assembly{"SETD", r5, r4, 1}
+                up <- assembly{"SETD", r5, r5, 0}
+                up <- assembly{"JUMPLABELIFEQ", end, r3, r5} // leave the loop if the val of r3 == the val of r5
+                up <- assembly{"SETD", r4, r4, 3}
+                up <- assembly{"SETD", r4, r4, 0}       // else move on to next link in chain
+                up <- assembly{"JUMPLABEL", loop, 0, 0}
+                up <- assembly{"LABEL", end, 0, 0}
+
+                for m := 1; m < members; m++ {
+                        up <- assembly{"NEW", r4, 4, 0}
+                        up <- assembly{"SETI", r4, 0, 1}
+                        up <- assembly{"SETI", r4, 1, 1}
+                        //up <- assembly{"SETI", r2, 7+m, 5}
+
                 // Inject args into env, descend registers, call closure
                 //up <- assembly{"REMEMBER-JUMP", r2, 3, sym[whatever]} // saves pc+1 to [r2]+3
         }
