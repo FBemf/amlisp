7252c97ee7bb8945c0c0f784b64d031e7fbd84c8
FBemf <felipe.m.p.bemfica@gmail.com>
2018-09-09 16:39:00 -0400

    Mostly added behaviour for the "func" func.
    Still gotta get it to return a closure.

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -4,6 +4,7 @@ package codegen
 import (
         "lexparse"
         "strconv"
+        "sync"
 )
 import "fmt"
 
@@ -12,20 +13,25 @@ func CodeGen(ast lexparse.Ast) {
 }
 
 // Symbol table of builtin funcs
-const masterSym map[string]int = map[string]int{
-        "_quote": 0,
-        "_+": 1,
-        "_-": 2,
-        "_cons": 3,
-        "_car": 4,
-        "_cdr": 5,
-        "_empty": 6,
-        "_if": 7,
-        "_define": 8,
-        "_func": 9
+const builtins map[string]string = map[string]string{
+        "quote": "internalQuote",
+        "+": "+",
+        "-": "-",
+        "cons": "cons",
+        "car": "car",
+        "cdr": "cdr",
+        "empty": "empty",
+        "if": "if",
+        "define": "define",
+        "FUNCTION": "func"
 }
 
-func call(up chan assembly, ast lexparse.Ast, sym map[string]int) {
+type safeSym struct {
+        table map[string]int
+        mutex sync.Mutex
+}
+
+func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) {
 
         if p := ast.Primitive(); p != nil {
                 switch p.Type() {
@@ -34,10 +40,43 @@ func call(up chan assembly, ast lexparse.Ast, sym map[string]int) {
                                 up <- assembly{"SET", r0, 0, 1} // refcount
                                 up <- assembly{"SET", r0, 1, Type_integer} // type
                                 up <- assembly{"SET", r0, 2, strconv.Atoi(p.Value())} // val
+                        case lexparse.Symbol:
+                                up <- assembly{"NEW", r0, 3, 0}
+                                up <- assembly{"SET", r0, 0, 1} // refcount
+                                up <- assembly{"SET", r0, 1, Type_symbol} // type
+                                name := strconv.Atoi(p.Value())
+                                sym.mutex.Lock()
+                                if id, ok := sym.table[name]; ok {
+                                        up <- assembly{"SET", r0, 2, id)} // val
+                                } else {
+                                        sym.table[name] = counter()
+                                        up <- assembly{"SET", r0, 2, sym.table[name])} // val
+                                }
+                                sym.mutex.Unlock()
                         default:
                                 fmt.Println("Unexpected primitive type!")
                         // TODO: add support for other primitives
                 }
+        }
+
+        var isFunc bool = false
+        if p := ast.Node().This().Primitive(); p.Type() == lexparse.Symbol {
+                if p.Value() == builtins["FUNCTION"] {
+                        isFunc = true
+                }
+        }
+
+        // If this is a function definition, defer execution
+        if isFunc {
+                funcStart := counter()
+                funcEnd := counter()
+                up <- assembly{"JUMP", funcEnd, 0, 0}
+                up <- assembly{"LABEL", funcStart, 0, 0}
+
+                // Also get rid of the arg list
+                ast = ast.Node().Next()
+                argAst := ast.Node().This()
+                ast = ast.Node().Next()
         }
 
         // Count members of s-exp
@@ -49,26 +88,30 @@ func call(up chan assembly, ast lexparse.Ast, sym map[string]int) {
         }
 
         // Make an env for it
-        // an env has refcount, type, length, saved pc, return loc, parent env, pointers
+        // an env has refcount, type, length, saved pc, return loc, parent env, symbol table, pointers
 
-        up <- assembly{"NEW", r2, members+6, 0}
+        // Note on symbols: A symbol is just a unique name. We'll need an internal function called RESOLVE
+        // that will dereference a symbol using the table and return a pointer directly to the value you want.
+        // DEFINE adds a new definition to the symbol table
+
+        up <- assembly{"NEW", r2, members+7, 0}
         up <- assembly{"SETID", r0, 0, r2}
         up <- assembly{"SETI", r2, 0, 1}
         up <- assembly{"SETI", r2, 1, Type_environment}
-        up <- assembly{"SETI", r2, 2, members + 4}
-
-        // SETP is like SET, but arg3 is a loc holding the number you want to set it to.
+        up <- assembly{"SETI", r2, 2, members+4}
         up <- assembly{"SETID", r2, 4, r0}
         up <- assembly{"SETID", r2, 5, r1}
+        up <- assembly{"SETD", r3, r1, 6}
+        up <- assembly{"SETD", r3, r3, 0}
+        up <- assembly{"SETID", r1, 6, r3}
         argCode := make([]chan assembly, members)
         for m := 0; m < members; m++ {
                 argCode[m] = make(chan assembly, 100)
-        for m := 0; m < members; m++ {
                 ast = ast.Node().Next()
-                go call(argCode[m], ast.Node.This(), copySym(sym))
+                go call(argCode[m], ast.Node().This(), counter, sym)
         }
         for m := 0; m < members; m++ {
-                up <- assembly{"SETD", r0, r2, 6+m}
+                up <- assembly{"SETD", r0, r2, 7+m}
                 up <- assembly{"SETD", r1, r2, 0}
                 for {
                         if a, b := <-argCode[m]; b {
@@ -79,20 +122,41 @@ func call(up chan assembly, ast lexparse.Ast, sym map[string]int) {
                 }
         }
 
-        //up <- assembly{"REMEMBER-JUMP", r2, 3, sym[whatever]} // saves pc+1 to [r2]+3
-        // TODO: work out how we're going to handle the symbol table
-        // We may have to make it a data structure
+        if isFunc {
+                up <- assembly{"SETD", r3, r2, members+6}
+                up <- assembly{"SETD", r3, r3, 0}
+                up <- assembly{"SETID", r0, 0, r3}      // return
 
-        // When we regain control, the return value is set
-        // decrement the refcount for this env
-        // if we hit zero, recursively do so for the pointers in this env
+                up <- assembly{"ADD1I", r3, 0, 0}       // add to refcount
 
-        // This block of code ascends the registers to the previous environment
-        up <- assembly{"SETD", r2, r1, 0}
-        up <- assembly{"SETD", r1, r2, 5}
-        up <- assembly{"SETD", r1, r1, 0}
-        up <- assembly{"SETD", r0, r2, 4}
-        up <- assembly{"SETD", r0, r0, 0}
+                up <- assembly{"SETD", r3, r2, 3}
+                up <- assembly{"SETD", r3, r3, 0}       // Grab the pc to return to
+
+                up <- assembly{"SUB1I", r2, 0, 0}       // decrement refcount
+
+                up <- assembly{"SETD", r4, r2, 0}
+                // Ascend the registers to the previous environment
+                up <- assembly{"SETD", r2, r1, 0}
+                up <- assembly{"SETD", r1, r2, 5}
+                up <- assembly{"SETD", r1, r1, 0}
+                up <- assembly{"SETD", r0, r2, 4}
+                up <- assembly{"SETD", r0, r0, 0}
+
+                up <- assembly{"REMEMBERJUMP", DUMPFUNC, r5, 0}
+                // where dumpfunc is the address of our garbage collector
+                // lemme outline how it works here: if the refcount of the place dumpfunc
+                // was called is 0, it runs dumpfunc on all of that place's pointers, then
+                // reduces that refcount to -1. Then the memory table will deallocate it
+                // the next time it sees it.
+
+                up <- assembly{"JUMPD", r3, 0, 0}      // jump back
+                up <- assembly{"LABEL", funcEnd, 0, 0}
+
+                // TODO: Should return a closure structure
+        } else {
+                // Inject args into env, descend registers, call closure
+                //up <- assembly{"REMEMBER-JUMP", r2, 3, sym[whatever]} // saves pc+1 to [r2]+3
+        }
 
         return
 }
@@ -109,6 +173,7 @@ const (
         Type_cons
         Type_vector
         Type_integer
+        Type_symbol
 )
 
 const (
@@ -119,11 +184,23 @@ const (
         r5
 )
 
-func copySym(sym map[string]int) (out map[string]int) {
+func makeCounter(i int) func() int {
+        var mux = &sync.Mutex{}
+        return func() int {
+                mux.Lock()
+                o := i
+                i++
+                mux.Unlock()
+                return o
+        }
+}
+
+
+/*func copySym(sym map[string]int) (out map[string]int) {
         out := make(map[string]int)
         for j, k := range(sym) {
                 out[j] = k
 
         }
         return
-}
+}*/
