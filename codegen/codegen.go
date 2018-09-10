48580278a6132eb4d42948c0bcdb4d3280ff8725
FBemf <felipe.m.p.bemfica@gmail.com>
2018-09-09 20:13:30 -0400

    Too tired to keep writing.
    Partway through putting args into a function

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -165,17 +165,19 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
                 }
 
                 // create and populate a closure to return
+                // contents of a closure: refcount, type, pc addr, env loc, length, args ...
                 up <- assembly{"NEW", r3, args+4, 0}
                 up <- assembly{"SETI", r3, 0, 1}
-                up <- assembly{"SETI", r3, 1, funcStart}
-                up <- assembly{"SETID", r3, 2, r2}
-                up <- assembly{"SETID", r3, 3, args+4}
+                up <- assembly{"SETI", r3, 1, Type_closure}
+                up <- assembly{"SETI", r3, 2, funcStart}
+                up <- assembly{"SETID", r3, 3, r2}
+                up <- assembly{"SETID", r3, 4, args+5}
                 for i := 0; i < args; i++ {
                         up <- assembly{"NEW", r4, 3, 0}
                         up <- assembly{"SETI", r4, 0, 1}
                         up <- assembly{"SETI", r4, 1, Type_symbol}
                         up <- assembly{"SETI", r4, 2, getSymID(argAst.Node().This().Primitive().Value(), counter)}
-                        up <- assembly{"SETID", r3, 4+i, r4}
+                        up <- assembly{"SETID", r3, 5+i, r4}
                 }
 
                 up <- assembly{"SETID", r0, 0, r3}      // return it
@@ -191,13 +193,14 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
                 up <- assembly{"DEREF", r0, r0, 0}
 
         } else {
-                up <- assembly{"SETD", r3, r2, 7}       // grab address with ptr to func sym
-                up <- assembly{"SETD", r3, r3, 0}       // grab address of func sym
+                up <- assembly{"SETD", r3, r2, 7}       // grab address with ptr to func symbol
+                up <- assembly{"DEREF", r3, r3, 0}       // grab address of func sym
                 up <- assembly{"SETD", r3, r3, 2}       // grab address of sym id
-                up <- assembly{"SETD", r3, r3, 0}       // grab sym id
+                up <- assembly{"DEREF", r3, r3, 0}       // grab sym id
 
                 up <- assembly{"SETD", r4, r2, 6}       // grab sym table loc
-                up <- assembly{"SETD", r4, r4, 0}       // grab sym table
+                up <- assembly{"DEREF", r4, r4, 0}       // grab sym table
+                up <- assembly{"SETD", r6, r4, 0}       // grab sym table
 
                 loop := counter()
                 end := counter()
@@ -205,20 +208,32 @@ func call(up chan assembly, ast lexparse.Ast, counter func() int, sym *safeSym) 
                 up <- assembly{"SETD", r5, r4, 1}
                 up <- assembly{"DEREF", r5, r5, 0}
                 up <- assembly{"JUMPLABELIFEQ", end, r3, r5} // leave the loop if the val of r3 == the val of r5
-                up <- assembly{"SETD", r4, r4, 3}
+                up <- assembly{"SETD", r4, r4, 4}
                 up <- assembly{"DEREF", r4, r4, 0}       // else move on to next link in chain
                 up <- assembly{"JUMPLABEL", loop, 0, 0}
                 up <- assembly{"LABEL", end, 0, 0}
 
                 up <- assembly{"SETD", r4, r4, 2}
-                up <- assembly{"DEREF", r4, r4, 0}
-                //change macro: _/SET T"cwDEREFf,;;dbi0
+                up <- assembly{"DEREF", r4, r4, 0}      // grab closure
 
                 for m := 1; m < members; m++ {
-                        up <- assembly{"NEW", r4, 4, 0}
-                        up <- assembly{"SETI", r4, 0, 1}
-                        up <- assembly{"SETI", r4, 1, 1}
-                        //up <- assembly{"SETI", r2, 7+m, 5}
+                        up <- assembly{"NEW", r5, 5, 0}
+                        up <- assembly{"SETI", r5, 0, 1}
+                        up <- assembly{"SETI", r5, 1, Type_symtab}
+                        up <- assembly{"SETD", r3, r4, 4}       // grab length of closure
+                        up <- assembly{"SUB", r3, 4, 0}         // subtract 4 from length to get number of args
+                        up <- assembly{"SET", r7, 0, 0}
+                        loop = counter()
+                        end = counter()
+                        up <- assembly{"LABEL", loop, 0, 0}
+                        // you now have a closure and a new symbol table cell.
+                        // iterate through the arguments in the closure in this loop
+                        // dereference them and set the ID in the symbol table cell to their IDs
+                        // then set the location to the contents of the appropriate cell in your current env.
+                        // Then pop on a new symtab cell and continue until r7 == r3
+                        // when this is done, make sure to wrap parts of this up as a builtin functions
+                        // all your functions are going to need to resolve symbols anyway.
+                        // you might even need it in this function
 
                 // Inject args into env, descend registers, call closure
                 //up <- assembly{"REMEMBER-JUMP", r2, 3, sym[whatever]} // saves pc+1 to [r2]+3
